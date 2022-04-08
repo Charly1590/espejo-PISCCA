da4db07d7a86ca088789d495acad9e84fa5868f6
FlorencioP <pfloro64@outlook.es>
2022-04-08 17:44:00 -0500

    agregacion de imagenes a la camara

---
+++ camara.py
@@ -0,0 +1,84 @@
+# import the opencv library
+import cv2
+
+import numpy as np
+from PIL import Image
+
+
+def overlay_image_alpha(img, img_overlay, x, y, alpha_mask):
+    """Overlay `img_overlay` onto `img` at (x, y) and blend using `alpha_mask`.
+
+    `alpha_mask` must have same HxW as `img_overlay` and values in range [0, 1].
+    """
+    # Image ranges
+    y1, y2 = max(0, y), min(img.shape[0], y + img_overlay.shape[0])
+    x1, x2 = max(0, x), min(img.shape[1], x + img_overlay.shape[1])
+
+    # Overlay ranges
+    y1o, y2o = max(0, -y), min(img_overlay.shape[0], img.shape[0] - y)
+    x1o, x2o = max(0, -x), min(img_overlay.shape[1], img.shape[1] - x)
+
+    # Exit if nothing to do
+    if y1 >= y2 or x1 >= x2 or y1o >= y2o or x1o >= x2o:
+        return
+
+    # Blend overlay within the determined ranges
+    img_crop = img[y1:y2, x1:x2]
+    img_overlay_crop = img_overlay[y1o:y2o, x1o:x2o]
+    alpha = alpha_mask[y1o:y2o, x1o:x2o, np.newaxis]
+    alpha_inv = 1.0 - alpha
+
+    img_crop[:] = alpha * img_overlay_crop + alpha_inv * img_crop
+
+
+
+# Save result
+#
+
+
+
+
+
+
+  
+# define a video capture object
+vid = cv2.VideoCapture(0)
+  
+while(True):
+      
+    # Capture the video frame
+    # by frame
+    ret, frame = vid.read()
+
+
+    # Prepare inputs
+    x, y = 100, 300
+    img = np.array(frame)
+    img_overlay_rgba = np.array(Image.open("cepillo.png"))
+
+
+
+    # Perform blending
+    alpha_mask = img_overlay_rgba[:, :, 3] / 255.0
+    img_result = img[:, :, :3].copy()
+    img_overlay = img_overlay_rgba[:, :, :3]
+
+    overlay_image_alpha(img_result, img_overlay, x, y, alpha_mask)
+
+
+    # Display the resulting frame
+    cv2.imshow('frame', frame)
+
+    cv2.imshow('cepillo', img_result)
+
+      
+    # the 'q' button is set as the
+    # quitting button you may use any
+    # desired button of your choice
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+  
+# After the loop release the cap object
+vid.release()
+# Destroy all the windows
+cv2.destroyAllWindows()

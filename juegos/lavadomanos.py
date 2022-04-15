677f86946b9b89065c4452196d848af844600d58
FlorencioP <pfloro64@outlook.es>
2022-04-14 19:18:11 -0500

    Avances en el juego de lavado de manos

--- juegos/lavadomanos.py
+++ juegos/lavadomanos.py
@@ -1,10 +1,25 @@
 import cv2
+from cv2 import log
 import mediapipe as mp
 import time 
 import numpy as np
 from PIL import Image
 from modulos.moduloPosicionarImgs import Posicionamiento as put_img
 import screeninfo
+import random
+
+
+
+mp_drawing = mp.solutions.drawing_utils
+
+#Banderas globales
+
+
+
+
+jabMano = False
+espuma=False
+manoR=False
 
 #Definicion de Pantalla para Pantalla completa
 screen = screeninfo.get_monitors()[0]
@@ -21,7 +36,7 @@ mp_pose = mp.solutions.pose
 
 
 #Captura de Video
-cap = cv2.VideoCapture(2)
+cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FOURCC,cv2.VideoWriter_fourcc('M','J','P','G'))
 cap.set(cv2.CAP_PROP_FRAME_WIDTH,1360)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT,768)
@@ -33,9 +48,9 @@ cap.set(cv2.CAP_PROP_FPS,60)
 #Graficos añadidos
 
 #Mano derecha
-img_mano_izquierda = np.array(Image.open("manosucia.png"))
+img_mano_izquierda = np.array(Image.open("recursos/autoc/lavmanos/las-bacterias.png"))
 img_mano_izquierda=cv2.rotate(img_mano_izquierda, cv2.ROTATE_90_COUNTERCLOCKWISE)
-img_mano_izquierda=cv2.resize(img_mano_izquierda,(50,50))
+img_mano_izquierda=cv2.resize(img_mano_izquierda,(100,120))
 #Creacion de Alpha
 alpha_mask_mano_izquierda= img_mano_izquierda[:, :, 3] / 255.0
 
@@ -46,9 +61,51 @@ alpha_mask_mano_izquierda= img_mano_izquierda[:, :, 3] / 255.0
 #Mano izquierda
 img_mano_derecha=cv2.flip(img_mano_izquierda, 0)
 #img_mano_izquierda=cv2.rotate(img_mano_derecha, cv2.ROTATE_90_COUNTERCLOCKWISE)
-img_mano_derecha=cv2.resize(img_mano_derecha,(50,50))
+img_mano_derecha=cv2.resize(img_mano_derecha,(120,100))
 #Creacion de Alpha
 alpha_mask_mano_derecha = img_mano_derecha[:, :, 3] / 255.0
+
+
+#Jabon
+tam = 2
+img_jabon = np.array(Image.open("recursos/autoc/lavmanos/jabon.png"))
+img_jabon=cv2.rotate(img_jabon, cv2.ROTATE_90_COUNTERCLOCKWISE)
+img_jabon=cv2.resize(img_jabon,(60*tam,80*tam))
+#Creacion de Alpha
+alpha_mask_jabon= img_jabon[:, :, 3] / 255.0
+
+#Burbujas
+img_burbuja = np.array(Image.open("recursos/autoc/lavmanos/burbuja.png"))
+img_burbuja=cv2.rotate(img_burbuja, cv2.ROTATE_90_COUNTERCLOCKWISE)
+img_burbuja=cv2.resize(img_burbuja,(100 ,100 ))
+#Creacion de Alpha
+alpha_mask_burbuja= img_burbuja[:, :, 3] / 255.0
+
+
+
+#Funcion para detectar el jabon 
+#Cuidado, aqui perdi el trazo de cual es izquierda o derecha por que el video esta invertido, pero la red neuronal no xd
+def tomar_jabon(xl, yl, xr, yr ):
+  global jabMano
+  global manoR
+  global espuma
+  
+  # 'Hitbox' del jabon 
+  if (xl>=1050 and xl<=1150) and (yl>=450  and yl<=550):
+    print('jabon der')
+    
+    jabMano=True
+    espuma=True
+
+  elif (xr>=1050 and xr<=1150) and (yr>=450  and yr<=550):
+    print('jabon izq')
+    
+    jabMano=True
+    manoR=True
+    espuma=True
+
+
+
 
 
 
@@ -90,15 +147,25 @@ with mp_pose.Pose(
     #Guardado de puntos de interes
     if results2.pose_landmarks:
 
-      #Guardado de posicion Mano Derecha
-      l_wrist_position_x=int(results2.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_WRIST].x*image_width)
-      l_wrist_position_y=int(results2.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_WRIST].y*image_height)
-      x, y =  l_wrist_position_x, l_wrist_position_y
+      #Guardado calculado de la posicion central Mano Derecha
+      l_position_x=int(results2.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_WRIST].x*image_width)
+      l_position_y=int(results2.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_WRIST].y*image_height)
+      xwl, ywl =  l_position_x, l_position_y
 
+      l_position_x=int(results2.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_INDEX].x*image_width)
+      l_position_y=int(results2.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_INDEX].y*image_height)
+      xil, yil =  l_position_x, l_position_y
 
-      #Reajuste de la posicion 
-      x-=100
-      y-=0
+      l_position_x=int(results2.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_PINKY].x*image_width)
+      l_position_y=int(results2.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_PINKY].y*image_height)
+      xpl, ypl =  l_position_x, l_position_y
+
+      xl = (xwl + xil + xpl) / 3
+      yl = (ywl + yil + ypl) / 3
+
+      #Reajuste de valores y la posicion 
+      xl=int(xl)-100
+      yl=int(yl)
 
       #Guardamos el Frame actual y dibujamos sobre la imagen
       img = np.array(image)
@@ -106,26 +173,84 @@ with mp_pose.Pose(
       img_overlay = img_mano_izquierda[:, :, :3]
       img_overlay = cv2.cvtColor(img_overlay, cv2.COLOR_BGR2RGB)
       #Metodo que recive la imagen base, la imagen a dibuja su posicion y alpha 
-      put_img.overlay_image_alpha(img_result, img_overlay, x, y, alpha_mask_mano_izquierda)
+      put_img.overlay_image_alpha(img_result, img_overlay, xl, yl, alpha_mask_mano_izquierda)
 
 
 
-      #Guardado de posicion Mano Izquierda
-      r_wrist_position_x=int(results2.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_WRIST].x*image_width)
-      r_wrist_position_y=int(results2.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_WRIST].y*image_height)
-      x, y = r_wrist_position_x, r_wrist_position_y
+     #Guardado calculado de la posicion central Mano Derecha
+      r_position_x=int(results2.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_WRIST].x*image_width)
+      r_position_y=int(results2.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_WRIST].y*image_height)
+      xwr, ywr =  r_position_x, r_position_y
 
-      #Reajuste de la posicion
-      x-=100
-      y-=0
+      r_position_x=int(results2.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_INDEX].x*image_width)
+      r_position_y=int(results2.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_INDEX].y*image_height)
+      xir, yir =  r_position_x, r_position_y
+
+      r_position_x=int(results2.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_PINKY].x*image_width)
+      r_position_y=int(results2.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_PINKY].y*image_height)
+      xpr, ypr =  r_position_x, r_position_y
+
+      xr = (xwr + xir + xpr) / 3
+      yr = (ywr + yir + ypr) / 3
+
+      #Reajuste de valores y la posicion 
+      xr=int(xr)-100
+      yr=int(yr)
+      
+      #Dibujado
+      img = np.array(img_result)
+      #img_result = img[:, :, :3].copy()
+      img_overlay = img_mano_derecha[:, :, :3]
+      img_overlay = cv2.cvtColor(img_overlay, cv2.COLOR_BGR2RGB)
+      put_img.overlay_image_alpha(img_result, img_overlay, xr, yr, alpha_mask_mano_derecha)
+
+
+      #Ajustes de la posicion del jabon y funcion
+      #Funciones
+      if(jabMano):
+        if(manoR):
+          xj = xr
+          yj = yr
+
+        else:
+          xj = xl
+          yj = yl
+
+      else:
+        xj = 1050
+        yj = 450 
+        tomar_jabon(xl, yl, xr, yr)
+          
+      if(espuma):
+        if (xl+150>=xr and xl-150<=xr) and (yl+150>=yr  and yl-150<=yr):
+          img = np.array(img_result)
+          #img_result = img[:, :, :3].copy()
+          img_overlay = img_burbuja[:, :, :3]
+          img_overlay = cv2.cvtColor(img_overlay, cv2.COLOR_BGR2RGB)
+          xbl = random.randint(xl-150, xl+150)
+          ybl = random.randint(yl-150, yl+150)
+          xbr = random.randint(xr-150, xr+150)
+          ybr = random.randint(yr-150, yr+150)
+          xb = int((xbl+xbr)/2)  
+          yb = int((ybl+ybr)/2) 
+
+          put_img.overlay_image_alpha(img_result, img_overlay, xb, yb, alpha_mask_burbuja)
+
+
 
       #Dibujado
       img = np.array(img_result)
-      img_result = img[:, :, :3].copy()
-      img_overlay = img_mano_derecha[:, :, :3]
+      #img_result = img[:, :, :3].copy()
+      img_overlay = img_jabon[:, :, :3]
       img_overlay = cv2.cvtColor(img_overlay, cv2.COLOR_BGR2RGB)
-      put_img.overlay_image_alpha(img_result, img_overlay, x, y, alpha_mask_mano_derecha)
+      put_img.overlay_image_alpha(img_result, img_overlay, xj, yj, alpha_mask_jabon) 
 
+
+      mp_drawing.draw_landmarks(img_result, results2.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+      
+
+
+    
 
 
     #Fin del contador de FPS
